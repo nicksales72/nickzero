0d1620a46f6e5e8bc992b5a9691eacb80e78d8f4
nicksales72 <nicksales72@gmail.com>
2025-02-25 12:55:18 -0500

    branches

--- src/chess_env.py
+++ src/chess_env.py
@@ -6,3 +6,10 @@ class Chess:
 
     def move_piece(self, move: str) -> None:
         self.board.push_san(move)
+
+    def is_legal_move(self, move: str) -> bool:
+        try:
+            parsed_move = self.board.parse_san(move)
+            return parsed_move in self.board.legal_moves
+        except ValueError:
+            return False
