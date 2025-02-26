6099268baa4a9d0f0ea7665807883fb5e4ce188c
nicksales72 <nicksales72@gmail.com>
2025-02-26 18:42:25 -0500

    implemented game loop

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from chess_env import Chess
+
+def game_loop(game):
+    while not game.game_over():
+        print(game.get_board_ascii())
+        turn = "White" if game.board.turn else "Black" # White == T, Black == F
+        move = input(f"{turn} to move: ")
+
+        if not game.move_piece(move): print("Invalid move.")
+
+    print(game.get_board_ascii())
+    print(f"{game.get_winner()} wins.")
+
+
+if __name__ == "__main__":
+    game = Chess()
+    game_loop(game)
