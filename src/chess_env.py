0b74ecaabbfd2c15c940201805a6bf277c2ecaf0
nicksales72 <nicksales72@gmail.com>
2025-02-26 16:16:06 -0500

    completed chess methods and tests

--- src/chess_env.py
+++ src/chess_env.py
@@ -4,26 +4,38 @@ class Chess:
     def __init__(self, board=None) -> None:
         self.board = board if board is not None else chess.Board()
 
-    def move_piece(self, move: str) -> None:
-        self.board.push_san(move)
+    def move_piece(self, move: str) -> bool:
+        if self.is_legal_move(move):
+            self.board.push_san(move)
+            return True
+        return False
+
+    def undo_move(self) -> bool:
+        if self.board.move_stack:
+            self.board.pop()
+            return True
+        return False
 
     def is_legal_move(self, move: str) -> bool:
-        try:
-            parsed_move = self.board.parse_san(move)
-            return parsed_move in self.board.legal_moves
-        except ValueError:
-            return False
+        return move in self.get_legal_moves()
+
+    def get_legal_moves(self) -> list:
+        return [self.board.san(move) for move in self.board.legal_moves]
+
+    def get_board_ascii(self) -> str:
+        return str(self.board)
+
+    def get_board_fen(self) -> str:
+        return self.board.fen()
+
+    def reset_board(self) -> None:
+        self.board.reset()
 
     def game_over(self) -> bool:
-        if self.board.is_checkmate():
-            return True
-        elif self.board.is_stalemate():
-            return True
-        elif self.board.is_insufficient_material():
-            return True
-        elif self.board.can_claim_fifty_moves():
-            return True
-        elif self.board.can_claim_threefold_repetition():
-            return True
-        else:
-            return False
+        return (
+            self.board.is_checkmate()
+            or self.board.is_stalemate()
+            or self.board.is_insufficient_material()
+            or self.board.can_claim_fifty_moves()
+            or self.board.can_claim_threefold_repetition()
+        )
--- tests/test_chess.py
+++ tests/test_chess.py
@@ -8,10 +8,14 @@ class TestChess(unittest.TestCase):
     def test_init_board(self):
         self.assertTrue(self.game.board.is_valid())
 
+        self.game.reset_board()
+
     def test_move(self):
         self.game.move_piece("e4")
         self.assertEqual(self.game.board.fullmove_number, 1)
         self.assertFalse(self.game.board.turn)
+
+        self.game.reset_board()
 
     def test_legal_move(self):
         self.assertTrue(self.game.is_legal_move("e4"))
@@ -20,13 +24,26 @@ class TestChess(unittest.TestCase):
         self.assertTrue(self.game.is_legal_move("Nc3"))
 
         self.assertFalse(self.game.is_legal_move("e5"))
-        self.assertTrue(self.game.is_legal_move("a3"))
         self.assertFalse(self.game.is_legal_move("Kd1"))
         self.assertFalse(self.game.is_legal_move("h9"))
         self.assertFalse(self.game.is_legal_move("Qh5"))
 
+        self.game.reset_board()
+
     def test_game_over(self):
         self.assertFalse(self.game.game_over())
+
+        # Scholars mate
+        self.game.move_piece("e4")
+        self.game.move_piece("e5")
+        self.game.move_piece("Qh5")
+        self.game.move_piece("Nc6")
+        self.game.move_piece("Bc4")
+        self.game.move_piece("Nf6")
+        self.game.move_piece("Qxf7#")
+        self.assertTrue(self.game.game_over()) 
+
+        self.game.reset_board()
 
 if __name__ == "__main__":
     unittest.main()
